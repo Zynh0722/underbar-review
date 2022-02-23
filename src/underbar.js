f4bed76eb0c13fb1453d8f16b59bbdaca332e43e
Walter Latimer <walter@walterlatimer.com>
2022-02-23 15:59:27 -0800

    Complete every

--- src/underbar.js
+++ src/underbar.js
@@ -1,3 +1,4 @@
+
 (function() {
   'use strict';
 
@@ -178,21 +179,26 @@
   _.reduce = function(collection, iterator, accumulator) {
     iterator = iterator || _.identity;
 
-    if (accumulator !== 0) { accumulator = accumulator || collection[0]; }
-
-
-    for (let i = (accumulator === collection[0]) ? 1 : 0; i < collection.length; i++) {
-      accumulator = iterator(accumulator, collection[i]);
+    if (accumulator === undefined) {
+      var memo = collection[0];
+      var newCollection = collection.slice(1);
+    } else {
+      var memo = accumulator;
+      var newCollection = collection;
     }
 
-    return accumulator;
+    _.each(newCollection, function(item) {
+      memo = iterator(memo, item);
+    });
+
+    return memo;
   };
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    return _.reduce(collection, function(wasFound, item) {
+    return _.reduce(collection, (wasFound, item) => {
       if (wasFound) {
         return true;
       }
@@ -204,6 +210,18 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    // direct inverse of contains
+
+    iterator = iterator || _.identity;
+
+    return _.reduce(collection, (wasFound, item) => {
+      if (!wasFound) {
+        return false;
+      }
+      return !!iterator(item);
+    }, true);
+
+    // reduce assume is true
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
